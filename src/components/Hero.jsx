49cf60a4cb5f6c370dca12fdc0ee5d37eefcbf42
NickRaise <nikhilnainwaya17@gmail.com>
2025-03-15 22:11:30 +0530

    completed the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,11 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Button from "./Button";
 import { TiLocationArrow } from "react-icons/ti";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger)
 
 const Hero = () => {
   const [currentIndex, setCurrentIndex] = useState(1);
@@ -26,6 +29,11 @@ const Hero = () => {
   const handleVideoLoad = () => {
     setLoadedVideo((prev) => prev + 1);
   };
+
+  useEffect(() => {
+    if(loadedVideo === totalVideos - 1)
+        setIsLoading(false)
+  }, [loadedVideo])
 
   useGSAP(() => {
     console.log('inside gsap')
@@ -51,8 +59,38 @@ const Hero = () => {
     }
   }, {dependencies: [currentIndex], revertOnUpdate: true})
 
+  useGSAP(() => {
+   gsap.set('#video-frame', {
+    clipPath: 'polygon(14% 0%, 72% 0%, 90% 90%, 0% 100%)',
+    borderRadius: '0 0 40% 10%'
+   }) 
+
+   gsap.from('#video-frame', {
+    clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)',
+    borderRadius: '0 0 0 0',
+    ease: 'power1.inOut',
+    scrollTrigger: {
+      trigger: '#video-frame',
+      start: 'center center',
+      end: 'bottom center',
+      scrub: true
+    }
+   })
+  })
+
   return (
     <div className="relative h-dvh w-screen overflow-x-hidden">
+
+      {isLoading &&
+        <div className="flex-center absolute z-[100] h-dvh w-screen bg-violet-50 overflow-hidden">
+          <div className="three-body">
+            <div className="three-body__dot"></div>
+            <div className="three-body__dot"></div>
+            <div className="three-body__dot"></div>
+          </div>
+        </div>  
+      }
+
       <div
         id="video-frame"
         className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75"
@@ -80,6 +118,7 @@ const Hero = () => {
             muted
             id="next-video"
             className="absolute-center invisible absolute z-20 size-64 object-cover object-center"
+            onLoadedData={handleVideoLoad}
           />
           <video
             src={getVideoSrc(currentIndex === totalVideos + 1 ? 1 : currentIndex)}

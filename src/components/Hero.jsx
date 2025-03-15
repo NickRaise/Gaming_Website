7dff31e12f4735db6d44a6aab159bb4f39110b09
NickRaise <nikhilnainwaya17@gmail.com>
2025-03-15 21:28:39 +0530

    herp section video animation done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,8 @@
 import React, { useRef, useState } from "react";
+import Button from "./Button";
+import { TiLocationArrow } from "react-icons/ti";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const Hero = () => {
   const [currentIndex, setCurrentIndex] = useState(1);
@@ -13,7 +17,7 @@ const Hero = () => {
 
   const handleMiniVideoClick = () => {
     setHasClicked(true);
-
+    console.log(currentIndex)
     setCurrentIndex(upcomingVideoIndex);
   };
 
@@ -22,6 +26,30 @@ const Hero = () => {
   const handleVideoLoad = () => {
     setLoadedVideo((prev) => prev + 1);
   };
+
+  useGSAP(() => {
+    console.log('inside gsap')
+    if(hasClicked) {
+      gsap.set('#next-video', {visibility: 'visible'})
+
+      gsap.to('#next-video', {
+        transformOrigin: 'center center',
+        scale: 1,
+        width: '100%',
+        height: '100%',
+        duration: 1,
+        ease: 'power1.inOut',
+        onStart: () => nextVideoRef.current.play()
+      })
+
+      gsap.from('#current-video', {
+        transformOrigin: 'center center',
+        scale: 0,
+        duration: 1.5,
+        ease: 'power1.inOut'
+      })
+    }
+  }, {dependencies: [currentIndex], revertOnUpdate: true})
 
   return (
     <div className="relative h-dvh w-screen overflow-x-hidden">
@@ -46,13 +74,13 @@ const Hero = () => {
               />
             </div>
           </div>
-          {/* <video
+          <video
             ref={nextVideoRef}
             src={getVideoSrc(currentIndex)}
             muted
             id="next-video"
-            className="absolute-center invisible absolute z-20 size-64 object-conver object-center"
-          /> */}
+            className="absolute-center invisible absolute z-20 size-64 object-cover object-center"
+          />
           <video
             src={getVideoSrc(currentIndex === totalVideos + 1 ? 1 : currentIndex)}
             muted
@@ -62,7 +90,29 @@ const Hero = () => {
             onLoadedData={handleVideoLoad}
           />
         </div>
+
+        <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75">
+          G<b>a</b>ming
+        </h1>
+
+        <div className="absolute top-0 left-0 z-40 size-full">
+          <div className="mt-24 px-5 sm:px-10">
+            <h1 className="special-font hero-heading text-blue-100">
+              redefi<b>n</b>e
+            </h1>
+            <p className="mb-5 max-w-64 font-robert-regular text-blue-100">
+              Enter the Metagame Layer <br /> Unleash the play Economy
+            </p>
+            <Button id="watch-trailer" title="Watch Trailer" leftIcon={<TiLocationArrow />} containerClass="!bg-yellow-300 flex-center gap-1" />
+          </div>
+        </div>
+
       </div>
+
+      <h1 className="special-font hero-heading absolute bottom-5 right-5 text-black">
+        G<b>a</b>ming
+      </h1>
+
     </div>
   );
 };

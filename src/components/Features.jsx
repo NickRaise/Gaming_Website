3ea7f259f8350f67f3134b2fe6c266a037b3132d
NickRaise <nikhilnainwaya17@gmail.com>
2025-03-21 13:07:01 +0530

    added the footer

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -21,7 +21,7 @@ const Features = () => {
             src="videos/feature-1.mp4"
             title={
               <>
-                radi<b>n</b>t
+                radia<b>n</b>t
               </>
             }
             description="A cross-platform meta app, turning your activities across Web2 and Web3 games into a rewarding adventure."

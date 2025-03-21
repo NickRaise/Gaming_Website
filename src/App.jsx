7b86c6be08f7b099097eb22e99297932850ab3a1
NickRaise <nikhilnainwaya17@gmail.com>
2025-03-21 12:59:36 +0530

    added the contact section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import About from "./components/About"
+import Contacts from "./components/Contacts"
 import Features from "./components/Features"
+import Footer from "./components/Footer"
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 import Story from "./components/Story"
@@ -13,6 +15,8 @@ function App() {
       <About />
       <Features />
       <Story />
+      <Contacts />
+      <Footer />
     </main>
   )
 }
